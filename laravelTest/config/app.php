9147e31120705cb4cbb28f9a56051794fc35bed4
paulrobins78 <paulrobins78@yahoo.it>
2015-04-27 09:53:17 +0200

    ...

--- laravelTest/config/app.php
+++ laravelTest/config/app.php
@@ -144,6 +144,7 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+        'Illuminate\Html\HtmlServiceProvider',
 
 	],
 
@@ -192,6 +193,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+        'Form'=> 'Illuminate\Html\FormFacade',
+        'HTML'=> 'Illuminate\Html\HtmlFacade'
 
 	],
 
